cc1c4e200dd9cb893a74c5398e5d6c7420f930b8
ringo32 <ringodekroon@gmail.com>
2020-10-05 00:15:13 +0200

    Update calamares_online.sh

--- .local/share/calamares-online/calamares_online.sh
+++ .local/share/calamares-online/calamares_online.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-
 sudo cp -praf /usr/share/calamares/settings.conf_online /usr/share/calamares/settings.conf
 sudo cp -praf /usr/share/calamares/modules/packages.conf_online /usr/share/calamares/modules/packages.conf
 sudo cp -praf /usr/share/calamares/modules/welcome.conf_online /usr/share/calamares/modules/welcome.conf
-
 lxterminal -e pkexec /usr/bin/calamares -d > /home/liveuser/endeavour-install.log
 cat /home/liveuser/endeavour-install.log | curl -F 'f:1=<-' ix.io
