3575fa25fde89b3278eeb26082d8420df08c9657
ringo32 <ringodekroon@gmail.com>
2020-10-06 10:27:52 +0200

    Update and rename calamares_online to calamares_online.sh

--- .local/share/calamares-online/calamares_online.sh
+++ .local/share/calamares-online/calamares_online.sh
@@ -2,5 +2,4 @@
 sudo cp -praf /usr/share/calamares/settings.conf_online /usr/share/calamares/settings.conf
 sudo cp -praf /usr/share/calamares/modules/packages.conf_online /usr/share/calamares/modules/packages.conf
 sudo cp -praf /usr/share/calamares/modules/welcome.conf_online /usr/share/calamares/modules/welcome.conf
-lxterminal -e sudo /usr/bin/calamares -d > /home/liveuser/endeavour-install.log
-cat /home/liveuser/endeavour-install.log | curl -F 'f:1=<-' ix.io
+
